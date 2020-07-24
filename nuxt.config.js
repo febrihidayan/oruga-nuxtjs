81878153aaf9a6633569a41f81dc78ff69393a65
Febri Hidayan <febrihidayan20@gmail.com>
2020-07-24 21:49:31 +0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ['@nuxt/typescript-build'],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@oruga-ui/oruga/nuxt'],
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <o-button @click="clickMe">Click Me</o-button>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    methods: {
+        clickMe() {
+            alert('Clicked!')
+        }
+    }
+})
+</script>
